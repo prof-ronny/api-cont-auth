b1128ddf8a05398885bf5b99d01f53feb8caf64d
prof-ronny <carlos.ronny@gmail.com>
2025-03-26 03:12:03 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB conectado'))
+  .catch(err => console.error('Erro ao conectar no MongoDB:', err));
+
+const usuarioRoutes = require('./routes/usuarioRoutes');
+const contatoRoutes = require('./routes/contatoRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
+
+app.use(express.json());
+app.use('/usuarios', usuarioRoutes);
+app.use('/contatos', contatoRoutes);
+app.use('/upload', uploadRoutes);
+
+module.exports = app;
