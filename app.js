3007d04b3ddd005a764c23611b9a5e2d4b6c0f89
prof-ronny <carlos.ronny@gmail.com>
2025-03-28 01:28:59 -0300

    Upload Corrigido

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
+const path = require('path');
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB conectado'))
@@ -12,8 +13,10 @@ const contatoRoutes = require('./routes/contatoRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
 
 app.use(express.json());
+
 app.use('/usuarios', usuarioRoutes);
 app.use('/contatos', contatoRoutes);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/upload', uploadRoutes);
 
 module.exports = app;
